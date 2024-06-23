4d351157f11d9d7259a461350b4eb1143e8e5fda
jabet <collegejcastersen@gmail.com>
2024-06-22 20:31:44 -0700

    on nameless req send start page

--- man_server.py
+++ man_server.py
@@ -8,6 +8,25 @@ PORT = 8000
 HOST = 'localhost'
 
 class ManPageHandler(http.server.SimpleHTTPRequestHandler):
+    def send_start_page(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        html_content = '''
+        <html>
+        <head><title>Man Page Server</title></head>
+        <body>
+            <h1>Man Page Server</h1>
+            <form action="/cgi-bin" method="get">
+                <label for="man2html">Enter Command:</label>
+                <input type="text" id="man2html" name="man2html">
+                <input type="submit" value="Get Man Page">
+            </form>
+        </body>
+        </html>
+        '''
+        self.wfile.write(html_content.encode('utf-8'))
+
     def parse_page_name(self, query: str):
         if '=' in query:
             return None, query.split('=')[-1]
@@ -26,7 +45,7 @@ class ManPageHandler(http.server.SimpleHTTPRequestHandler):
             print(f'Section: {section}, Name: {name}')
 
             if not name:
-                self.send_error(400, 'Please provide a name for the man page.')
+                self.send_start_page()
                 return
 
             man_page_html = post_process_page(convert_page(find_page(name, section)))
@@ -40,24 +59,7 @@ class ManPageHandler(http.server.SimpleHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(man_page_html.encode('utf-8'))
         else:
-            # Handle the root path
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            html_content = '''
-            <html>
-            <head><title>Man Page Server</title></head>
-            <body>
-                <h1>Man Page Server</h1>
-                <form action="/cgi-bin" method="get">
-                    <label for="man2html">Enter Command:</label>
-                    <input type="text" id="man2html" name="man2html">
-                    <input type="submit" value="Get Man Page">
-                </form>
-            </body>
-            </html>
-            '''
-            self.wfile.write(html_content.encode('utf-8'))
+            self.send_start_page()
             return
 
 with socketserver.TCPServer((HOST, PORT), ManPageHandler) as server:
