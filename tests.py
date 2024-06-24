d54ce99fdb1d6db5b814fe634ede78359b9ff61f
jabet <collegejcastersen@gmail.com>
2024-06-24 15:28:39 -0700

    Remove request dependency

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
-import requests
 import socketserver
 import threading
+import urllib.request
 
 from man_parser import get_page, _page_in_cache, Potentials
 from man_server import ManPageHandler
@@ -27,39 +27,40 @@ class TestFindPage(unittest.TestCase):
 class ManTCPTestServer(socketserver.TCPServer):
     allow_reuse_address = True
 
+PORT = 8000
 class TestServer(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.server = ManTCPTestServer(('localhost', 8000), ManPageHandler)
+        cls.server = ManTCPTestServer(('localhost', PORT), ManPageHandler)
         cls.server_thread = threading.Thread(target=cls.server.serve_forever)
         cls.server_thread.daemon = True
         cls.server_thread.start()
 
     def test_startup_page_default(self):
-        response = requests.get('http://localhost:8000')
-        self.assertEqual(response.status_code, 200)
-        page_bytes = bytes(StartPage.start_page.replace(ERROR_KEY, '').encode('utf-8'))
-        self.assertEqual(response.content, page_bytes)
+        with urllib.request.urlopen(f'http://localhost:{PORT}') as response:
+            self.assertEqual(response.status, 200)
+            page_bytes = bytes(StartPage.start_page.replace(ERROR_KEY, '').encode('utf-8'))
+            self.assertEqual(response.read(), page_bytes)
 
     def test_startup_page_error(self):
-        response = requests.get('http://localhost:8000/cgi-bin/man/man2html?10+fake')
-        self.assertEqual(response.status_code, 200)
-        error_msg = 'Could not find man page for fake section 10' 
-        error_bytes = bytes(StartPage.start_page.replace(ERROR_KEY, error_msg).encode('utf-8'))
-        self.assertEqual(response.content, error_bytes)
+        with urllib.request.urlopen(f'http://localhost:{PORT}/cgi-bin/man/man2html?10+fake') as response:
+            self.assertEqual(response.status, 200)
+            error_msg = 'Could not find man page for fake section 10' 
+            error_bytes = bytes(StartPage.start_page.replace(ERROR_KEY, error_msg).encode('utf-8'))
+            self.assertEqual(response.read(), error_bytes)
 
     def test_startup_page_no_name(self):
-        response = requests.get('http://localhost:8000/cgi-bin/man/man2html?10')
-        self.assertEqual(response.status_code, 200)
-        error_msg = 'Please provide man page name' 
-        error_bytes = bytes(StartPage.start_page.replace(ERROR_KEY, error_msg).encode('utf-8'))
-        self.assertEqual(response.content, error_bytes)
+        with urllib.request.urlopen(f'http://localhost:{PORT}/cgi-bin/man/man2html?10') as response:
+            self.assertEqual(response.status, 200)
+            error_msg = 'Please provide man page name' 
+            error_bytes = bytes(StartPage.start_page.replace(ERROR_KEY, error_msg).encode('utf-8'))
+            self.assertEqual(response.read(), error_bytes)
 
     def test_valid_page(self):
-        response = requests.get('http://localhost:8000/cgi-bin/man/man2html?1+man')
-        self.assertEqual(response.status_code, 200)
-        converted_man_page = bytes(get_page('man', '1', DEFAULT_THEME).encode('utf-8'))
-        self.assertEqual(response.content, converted_man_page)
+        with urllib.request.urlopen(f'http://localhost:{PORT}/cgi-bin/man/man2html?1+man') as response:
+            self.assertEqual(response.status, 200)
+            converted_man_page = bytes(get_page('man', '1', DEFAULT_THEME).encode('utf-8'))
+            self.assertEqual(response.read(), converted_man_page)
 
     @classmethod
     def tearDownClass(cls):
