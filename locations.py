50dc85b098b488d58699c8bb5bc11f2b919d15ce
jabet <collegejcastersen@gmail.com>
2024-06-24 01:33:40 -0700

    Only compute themes once

--- locations.py
+++ locations.py
@@ -4,15 +4,15 @@ STARTUP_PAGE = 'startup_page.html'
 TEMPLATE_PAGE = 'template.html'
 ERROR_KEY = '{error}'
 
-THEME_DIR = 'themes'
-DEFAULT_THEME = Path(THEME_DIR) / 'default.css'
+THEME_DIR = Path('themes')
+THEMES = [theme for theme in THEME_DIR.iterdir()]
+DEFAULT_THEME = THEME_DIR / 'default.css'
 THEME_KEY = '/*Theme*/'
 
 def add_theme(page: str, theme_name: str):
-    themes = [str(theme) for theme in Path(THEME_DIR).iterdir()]
     theme_name = __format_theme_name(theme_name)
 
-    if not theme_name or theme_name not in themes:
+    if not theme_name or theme_name not in THEMES:
         return __add_theme(page, DEFAULT_THEME)
 
     return __add_theme(page, theme_name)
@@ -25,7 +25,7 @@ def __format_theme_name(theme_name: str):
     if not theme_name:
         return None
 
-    return THEME_DIR + '/' + theme_name + '.css'
+    return THEME_DIR / (theme_name + '.css')
 
 def get_page_contents(page):
     with open(page, 'r') as f:
